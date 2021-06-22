9dd6ca1d9ec1d02b05323574d101b5f77849066a
JD Wilson <jaydee.wilson@gmail.com>
2021-06-22 13:45:25 +0200

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var slider = document.getElementById("slider");
+var output = document.getElementById("output");
+output.innerHTML = slider.value; 
+
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
